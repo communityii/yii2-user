d1b0ed8f7de19ad9b4f3724c34a452ef02a257e1
Kartik Visweswaran <kartikv2@gmail.com>
2014-03-31 15:30:33 +0530

    Identity interface skeleton

---
+++ user/components/IdentityInterface.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @copyright Copyright &copy; communityii, 2014
+ * @package yii2-user
+ * @version 1.0.0
+ * @see https://github.com/communityii/yii2-user
+ */
+
+namespace communityii\user\components\IdentityInterface;
+
+use Yii;
+use communityii\user\models\User;
+
+/**
+ * User identity interface
+ *
+ * @author Kartik Visweswaran <kartikv2@gmail.com>
+ * @since 1.0
+ */
+class IdentityInterface extends \yii\web\IdentityInterface
+{
+
+}
