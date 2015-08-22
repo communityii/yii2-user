0102a7ffb696a1d7ee1c6e34ded639a95ed613fc
Kartik Visweswaran <kartikv2@gmail.com>
2015-08-22 09:52:50 +0530

    Updates to migrations

--- migrations/m140402_143411_create_user_module.php
+++ migrations/m140402_143411_create_user_module.php
@@ -6,9 +6,7 @@
  * @version 1.0.0
  * @see https://github.com/communityii/yii2-user
  */
-
-use yii\db\Schema;
-use comyii\user\Module;
+use yii\db\Migration;
 
 /**
  * Migration for creating the database structure for the comyii\yii2-user module.
@@ -16,11 +14,13 @@ use comyii\user\Module;
  * @author Kartik Visweswaran <kartikv2@gmail.com>
  * @since 1.0
  */
-class m140402_143411_create_user_module extends \yii\db\Migration
+class m140402_143411_create_user_module extends Migration
 {
     public function up()
     {
         $tableOptions = null;
+        $timestamp = $this->timestamp()->notNull()->defaultValue('0000-00-00 00:00:00');
+
         if ($this->db->driverName === 'mysql') {
             $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_general_ci ENGINE=InnoDB';
         }
@@ -28,35 +28,40 @@ class m140402_143411_create_user_module extends \yii\db\Migration
         // Table # 1: User
         $this->createTable('{{%user}}', [
             'id' => $this->bigPrimaryKey(),
-            'username' => $this->string(30)->notNull(),
-            'email' => $this->string(255)->notNull(),
+            'username' => $this->string(30)->notNull()->unique(),
+            'email' => $this->string(255)->notNull()->unique(),
             'password' => $this->string(30)->notNull(),
             'auth_key' => $this->string(128)->notNull(),
             'activation_key' => $this->string(128),
             'reset_key' => $this->string(128),
             'status' => $this->smallInteger()->notNull()->defaultValue(0),
-            'password_fail_attempts' => $this->smallInteger()->defaultValue(0),
-            'last_login_ip' => $this->string(50),
-            'last_login_on' => $this->timestamp()->notNull()->defaultValue('0000-00-00 00:00:00'),
-            'password_reset_on' => $this->timestamp()->notNull()->defaultValue('0000-00-00 00:00:00'),
-            'created_on' => $this->timestamp()->notNull()->defaultValue('0000-00-00 00:00:00'),
-            'updated_on' => $this->timestamp()->notNull()->defaultValue('0000-00-00 00:00:00'),
+            'pwd_fail_attempts' => $this->smallInteger()->defaultValue(0),
+            'pwd_reset_on' => $timestamp,
+            'created_on' => $timestamp,
+            'updated_on' => $timestamp,
+            'last_login_on' => $timestamp,
+            'last_login_ip' => $this->string(50)
         ], $tableOptions);
-        $this->createIndex('user_username_uk', '{{%user}}', 'username', true);
-        $this->createIndex('user_email_uk', '{{%user}}', 'email', true);
-        $this->createIndex('user_status_nu', '{{%user}}', 'status');
+        $this->createIndex('user_status_idx', '{{%user}}', 'status');
 
         // Table # 2: Remote identity
         $this->createTable('{{%remote_identity}}', [
-            'id' => $this->bigInteger(),
+            'id' => $this->bigPrimaryKey(),
             'profile_id' => $this->string(128)->notNull(),
             'provider' => $this->string(30)->notNull(),
             'user_id' => $this->bigInteger()->notNull(),
-            'created_on' => $this->timestamp()->notNull()->defaultValue('0000-00-00 00:00:00'),
-            'updated_on' => $this->timestamp()->notNull()->defaultValue('0000-00-00 00:00:00'),
+            'created_on' => $timestamp,
+            'updated_on' => $timestamp,
         ], $tableOptions);
-        $this->addForeignKey('remote_identity_fk', '{{%remote_identity}}', 'user_id', '{{%user}}', 'id', 'CASCADE');
-        $this->createIndex('remote_identity_nu', '{{%remote_identity}}', 'user_id');
+        $this->addForeignKey(
+            'remote_identity_user_id_fk',
+            '{{%remote_identity}}',
+            'user_id',
+            '{{%user}}',
+            'id',
+            'CASCADE'
+        );
+        $this->createIndex('remote_identity_user_id_idx', '{{%remote_identity}}', 'user_id');
 
         // Table # 3: User profile
         $this->createTable('{{%user_profile}}', [
@@ -65,11 +70,18 @@ class m140402_143411_create_user_module extends \yii\db\Migration
             'first_name' => $this->string(60),
             'last_name' => $this->string(60),
             'avatar_url' => $this->text(),
-            'created_on' => $this->timestamp()->notNull()->defaultValue('0000-00-00 00:00:00'),
-            'updated_on' => $this->timestamp()->notNull()->defaultValue('0000-00-00 00:00:00'),
+            'created_on' => $timestamp,
+            'updated_on' => $timestamp,
         ], $tableOptions);
-        $this->addPrimaryKey('user_profile_pk', '{{%user_profile}}', 'id');
-        $this->addForeignKey('user_profile_fk', '{{%user_profile}}', 'id', '{{%user}}', 'id', 'CASCADE');
+        $this->addPrimaryKey('user_profile_user_id_pk', '{{%user_profile}}', 'id');
+        $this->addForeignKey(
+            'user_profile_user_id_fk',
+            '{{%user_profile}}',
+            'id',
+            '{{%user}}',
+            'id',
+            'CASCADE'
+        );
 
         // Table # 4: User ban log
         $this->createTable('{{%user_ban_log}}', [
@@ -79,27 +91,34 @@ class m140402_143411_create_user_module extends \yii\db\Migration
             'revoke_reason' => $this->string(255),
             'user_id' => $this->bigInteger()->notNull(),
             'banned_till' => $this->timestamp()->defaultValue('0000-00-00 00:00:00'),
-            'created_on' => $this->timestamp()->notNull()->defaultValue('0000-00-00 00:00:00'),
-            'updated_on' => $this->timestamp()->notNull()->defaultValue('0000-00-00 00:00:00'),
+            'created_on' => $timestamp,
+            'updated_on' => $timestamp,
         ], $tableOptions);
-        $this->addForeignKey('user_ban_log_fk', '{{%user_ban_log}}', 'user_id', '{{%user}}', 'id', 'CASCADE');
-        $this->createIndex('user_ban_log_nu}}', '{{%user_ban_log}}', 'user_id');
+        $this->addForeignKey(
+            'user_ban_log_user_id_fk',
+            '{{%user_ban_log}}',
+            'user_id',
+            '{{%user}}',
+            'id',
+            'CASCADE'
+        );
+        $this->createIndex('user_ban_log_user_id_idx}}', '{{%user_ban_log}}', 'user_id');
 
         // Table # 5: Mail queue
         $this->createTable('{{%mail_queue}}', [
             'id' => $this->bigPrimaryKey(),
-            'from_email' => $this->string(255)->notNull  (),
+            'from_email' => $this->string(255)->notNull(),
             'from_name' => $this->string(255),
             'subject' => $this->string(255)->notNull(),
             'template' => $this->string(60)->notNull(),
             'user_id' => $this->bigInteger()->notNull(),
             'mail_log' => $this->text(),
             'status' => $this->smallInteger()->notNull()->defaultValue(0),
-            'created_on' => $this->timestamp()->notNull()->defaultValue('0000-00-00 00:00:00'),
-            'updated_on' => $this->timestamp()->notNull()->defaultValue('0000-00-00 00:00:00'),
+            'created_on' => $timestamp,
+            'updated_on' => $timestamp,
         ], $tableOptions);
-        $this->addForeignKey('mail_queue_fk', '{{%mail_queue}}', 'user_id', '{{%user}}', 'id', 'CASCADE');
-        $this->createIndex('mail_queue_nu', '{{%mail_queue}}', 'user_id');
+        $this->addForeignKey('mail_queue_user_id_fk', '{{%mail_queue}}', 'user_id', '{{%user}}', 'id', 'CASCADE');
+        $this->createIndex('mail_queue_user_id_idx', '{{%mail_queue}}', 'user_id');
     }
 
     public function down()
