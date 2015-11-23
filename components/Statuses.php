d2e25fcc15d46a107b06efb9edcb268d8a29bafd
derekisbusy <derekisbusy@gmail.com>
2015-11-22 23:29:00 -0500

    Updated Statuses component

--- components/Statuses.php
+++ components/Statuses.php
@@ -11,50 +11,83 @@
 
 namespace comyii\user\components;
 
-use yii\base\Component;
-use comyii\user\Module;
+use Yii;
+use comyii\user\components\ArrayComponent;
 
 /**
  * Class Models the status settings for the module.
  * 
  * @package comyii\user\components
  */
-class Statuses extends Component
+class Statuses extends ArrayComponent
 {
-    public $statuses = [
-        Module::MODEL_LOGIN => 'comyii\user\models\LoginForm',
-        Module::MODEL_USER => 'comyii\user\models\User',
-        Module::MODEL_USER_SEARCH => 'comyii\user\models\UserSearch',
-        Module::MODEL_PROFILE => 'comyii\user\models\UserProfile',
-        Module::MODEL_SOCIAL_PROFILE => 'comyii\user\models\SocialProfile',
-        Module::MODEL_PROFILE_SEARCH => 'comyii\user\models\UserProfileSearch',
-        Module::MODEL_RECOVERY => 'comyii\user\models\RecoveryForm',
+    const STATUS_SUPERUSER = -1;
+    const STATUS_PENDING = 0;
+    const STATUS_ACTIVE = 1;
+    const STATUS_INACTIVE = 2;
+    const STATUS_ADMIN = 3;
+    const STATUS_EXPIRED = 4;
+    const STATUS_LOCKED = 5;
+
+    /**
+     * @var string the name of the property to store the array items 
+     */
+    protected $_containerName = 'statuses';
+    protected $_currentName = 'status';
+
+    /**
+     * @var array the list of layouts 
+     */
+    public $statuses;
+
+    public $status;
+
+    /**
+     * @var array the user statuses which are internal to system and not
+     * available for direct update by admin or superuser
+     */
+    protected $internal = [
+        self::STATUS_SUPERUSER,
+        self::STATUS_PENDING
     ];
 
     /**
-     * Constructor for Statuses component
-     * 
-     * @param type $config
+     * @var array the user statuses which are secondary
      */
-    public function __construct($config = array(), $m = null) {
-        if ($m === null) {
-            $m = Yii::$app->getModule('user');
-        }
-        $this->statuses = [
-            Module::STATUS_SUPERUSER => Yii::t('user', 'Superuser'),
-            Module::STATUS_PENDING => Yii::t('user', 'Pending'),
-            Module::STATUS_ACTIVE => Yii::t('user', 'Active'),
-            Module::STATUS_INACTIVE => Yii::t('user', 'Inactive'),
-            Module::STATUS_ADMIN => Yii::t('user', 'Admin'),
-            Module::STATUS_LOCKED => Yii::t('user', 'Locked'),
-            Module::STATUS_EXPIRED => Yii::t('user', 'Expired'),
+    protected $secondary = [
+        self::STATUS_LOCKED,
+        self::STATUS_EXPIRED,
+    ];
+
+    /**
+     * @var array the CSS classes for displaying user status as HTML
+     */
+    public $classes = [
+        self::STATUS_SUPERUSER => 'label label-primary',
+        self::STATUS_PENDING => 'label label-warning',
+        self::STATUS_ACTIVE => 'label label-success',
+        self::STATUS_INACTIVE => 'label label-danger',
+        self::STATUS_ADMIN => 'label label-info',
+        self::STATUS_EXPIRED => 'label label-default',
+        self::STATUS_LOCKED => 'label label-danger',
+    ];
+
+    /**
+     * Get the default actions
+     * 
+     * @return array
+     */
+    public function getDefaults()
+    {
+        return [
+            self::STATUS_SUPERUSER => Yii::t('user', 'Superuser'),
+            self::STATUS_PENDING => Yii::t('user', 'Pending'),
+            self::STATUS_ACTIVE => Yii::t('user', 'Active'),
+            self::STATUS_INACTIVE => Yii::t('user', 'Inactive'),
+            self::STATUS_ADMIN => Yii::t('user', 'Admin'),
+            self::STATUS_LOCKED => Yii::t('user', 'Locked'),
+            self::STATUS_EXPIRED => Yii::t('user', 'Expired'),
         ];
-        if ($m->userTypes[$m->getUserType()]['models']) {
-            $config = Module::mergeDefault($config, $m->userTypes[$m->getUserType()]['statuses']);
-        }
-        if (isset($config['statuses'])) {
-            $this->statuses = array_replace_recursive($this->statuses, $config['statuses']);
-        }
     }
 
     /**
@@ -62,7 +95,7 @@ class Statuses extends Component
      *
      * @return string
      */
-    public function getValidStatuses()
+    public function getValid()
     {
         $statuses = [];
         $exclude1 = array_flip($this->secondaryStatuses);
@@ -80,10 +113,10 @@ class Statuses extends Component
      *
      * @return string
      */
-    public function getEditStatuses()
+    public function getEdit()
     {
         $statuses = [];
-        $exclude = array_flip($this->secondaryStatuses);
+        $exclude = array_flip($this->secondary);
         foreach ($this->statuses as $status => $name) {
             if (!isset($exclude[$status])) {
                 $statuses[$status] = $name;
@@ -97,10 +130,10 @@ class Statuses extends Component
      *
      * @return array
      */
-    public function getDisabledStatuses()
+    public function getDisabled()
     {
         $options = [];
-        foreach ($this->internalStatuses as $status) {
+        foreach ($this->internal as $status) {
             $options[$status] = ['disabled' => true];
         }
         return $options;
@@ -111,10 +144,10 @@ class Statuses extends Component
      *
      * @return string
      */
-    public function getPrimaryStatuses()
+    public function getPrimary()
     {
         $statuses = [];
-        $exclude = array_flip($this->secondaryStatuses);
+        $exclude = array_flip($this->secondary);
         foreach ($this->statuses as $status => $name) {
             if (!isset($exclude[$status])) {
                 $statuses[$status] = $name;
@@ -128,10 +161,10 @@ class Statuses extends Component
      *
      * @return string
      */
-    public function getSecondaryStatuses()
+    public function getSecondary()
     {
         $statuses = [];
-        foreach ($this->secondaryStatuses as $status) {
+        foreach ($this->secondary as $status) {
             $statuses[$status] = $this->statuses[$status];
         }
         return $statuses;
